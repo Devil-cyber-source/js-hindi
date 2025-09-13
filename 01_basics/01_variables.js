1e05258a2ec82d31f9d3fd4886f7be38ba805a25
Devil-cyber-source <83034227+Devil-cyber-source@users.noreply.github.com>
2025-09-13 15:27:25 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 2334223  // never be change
+let accountEmail = "ankit@gmail.com"
+var accountPassword = "298930"
+accountCity ="delhi"
+let accountState ;
+
+// accountId = 2 not allowed 
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+
+*/
+
+accountEmail = "hhsjhdj@gmail.com"
+accountPassword = "129839"
+accountCity = "jaipur"
+
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
